e7545a043b40ef2b67d404837482e2ce9ec943cf
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-08-11 00:20:34 -0400

    feat: initial repo

---
+++ src/main/java/edu/northeastern/controller/MessagingController.java
@@ -0,0 +1,27 @@
+package edu.northeastern.controller;
+
+import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import edu.northeastern.model.Message;
+
+@Controller
+public class MessagingController {
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public Message register(@Payload Message chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        var attributes = headerAccessor.getSessionAttributes();
+        if (attributes != null) {
+            attributes.put("username", chatMessage.getSender());
+        }
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public Message sendMessage(@Payload Message chatMessage) {
+        return chatMessage;
+    }
+}
---
+++ src/main/java/edu/northeastern/model/Message.java
@@ -0,0 +1,25 @@
+package edu.northeastern.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    /**
+     * Enum representing the type of the chat message.
+     */
+    public enum MessageType {
+        CHAT, LEAVE, JOIN
+    }
+}
